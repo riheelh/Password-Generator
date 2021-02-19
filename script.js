6f1355096db4daedc4deef4be92820a6ee790557
Riheel Hamoande <riheel83@gmail.com>
2021-02-19 17:11:51 -0600

    Refactor and orgnize the code and ReadMe

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
 // // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
-
-
 // Write password to the #password input
 function writePassword() {
     //prompt for size of the password
-    let n = prompt('Type number of charcters for your password (min=8 max=128)')
+    let n = prompt('Type number of charcters for your password (min=8 max=128)');
     //validate the number of charcters are min 8 and max 128 chars
     if (!(n >= 8 && n <= 128 ) || n === null) {
       return alert('Does not meet password criteria (min=8 max=128), start again');
     }
     
     // Confirm the criteria selection True and false
-    let lowerCaseConf = confirm('Would you like to include lower case charcters ?')
-    console.log(lowerCaseConf)
+    let lowerCaseConf = confirm('Would you like to include lower case charcters ?');
     
-    let upperCaseConf = confirm('Would you like to include upper case charcters ?')
-    console.log(upperCaseConf)
+    let upperCaseConf = confirm('Would you like to include upper case charcters ?');
     
-    let numConf = confirm('Would you like to include number ?')
-    console.log(numConf)
-    
-    let spclConf = confirm('Would you like to include special charcters ?')
-    console.log(spclConf)
+    let numConf = confirm('Would you like to include number ?');
+
+    let spclConf = confirm('Would you like to include special charcters ?');
 
     // validate at least one character criteria selected (at least 1 true)
     if (lowerCaseConf === false && upperCaseConf === false && numConf === false && spclConf === false) {
@@ -53,34 +45,27 @@ function writePassword() {
     function generatePassword() {
       // declare empty var for password
       var Pass = ""
-      var somethingChar = ""
+      var finalPass = ""
       // this is the loop building the pass char by char, this password length would be 16 chars
       for (var i=0; i<n; i++) {
         
-        // each time in the loop generate a number between 0 to 3
-        decisionmaker = Math.floor(Math.random() * 4) 
+        // Randomize the picking of types
+        randomFunc = Math.floor(Math.random() * 4) 
         
-        if (decisionmaker === 0 && lowerCaseConf === true) {somethingChar =randLower()} else 
-        if (decisionmaker === 1 && upperCaseConf === true) {somethingChar =randUpper()} else
-        if (decisionmaker === 2 && numConf === true) {somethingChar =randNumber()} else 
-        if (decisionmaker === 3 && spclConf === true) {somethingChar =randSpecial()}
+        if (randomFunc === 0 && lowerCaseConf === true) {finalPass =randLower()}
+        if (randomFunc === 1 && upperCaseConf === true) {finalPass =randUpper()}
+        if (randomFunc === 2 && numConf === true) {finalPass =randNumber()} 
+        if (randomFunc === 3 && spclConf === true) {finalPass =randSpecial()}
 
-        Pass += somethingChar
-    
-      } // loop end
-
+        Pass += finalPass
+      } 
        return Pass
     }
-
-      //console.log(generatePassword())
-      // append chars to initial pass in array 
-
       //loop throug the chars type functions with number of chars
       var password = generatePassword();
       var passwordText = document.querySelector("#password");
 
       passwordText.value = password;
-
 }
 
 // Add event listener to generate button
